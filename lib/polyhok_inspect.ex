cbe08931fbdff0f5dd9ff0f344eaaad6ce9d02ca
jans@ubuntu <jansoares@inf.ufpel.edu.br>
2025-11-26 09:55:26 -0300

    Adding a module to inspect the AST generated by PolyHok internal macros.

---
+++ lib/polyhok_inspect.ex
@@ -0,0 +1,77 @@
+defmodule PolyHokInspect do
+  defp expand_all_macro_ast(ast, %Macro.Env{} = env) do
+    Macro.prewalk(ast, fn node ->
+      Macro.expand(node, env)
+    end)
+  end
+
+  defmacro block_inspect(do: block) do
+    original_ast = block
+    expanded_once = Macro.expand_once(block, __CALLER__)
+    expanded_ast = expand_all_macro_ast(block, __CALLER__)
+    original_ast_pretty = inspect(original_ast, pretty: true, limit: :infinity)
+    expanded_ast_pretty = inspect(expanded_ast, pretty: true, limit: :infinity)
+
+    generated_code =
+      expanded_ast
+      |> Macro.to_string()
+      |> Code.format_string!()
+      |> IO.iodata_to_binary()
+
+    generated_code_normal = Macro.to_string(expanded_once)
+
+    IO.puts("""
+
+    \n=== MacroDbg ===
+    -- AST (original):
+    #{original_ast_pretty}
+
+    -- AST (expandida):
+    #{expanded_ast_pretty}
+
+    -- Código gerado (após expansão):
+    #{generated_code}
+    === /MacroDbg ===
+
+    """)
+
+    IO.puts("Codigo normal:
+      #{generated_code_normal}
+      ")
+
+    block
+  end
+  defmacro block_inspect_when_polyhok(do: block) do
+        original_ast = block
+    expanded_once = Macro.expand_once(block, __CALLER__)
+    expanded_ast = expand_all_macro_ast(block, __CALLER__)
+    original_ast_pretty = inspect(original_ast, pretty: true, limit: :infinity)
+    expanded_ast_pretty = inspect(expanded_ast, pretty: true, limit: :infinity)
+    generated_code =
+      expanded_ast
+      |> Macro.to_string()
+
+    generated_code_normal = Macro.to_string(expanded_once)
+
+    IO.puts("""
+
+    \n=== MacroDbg ===
+    -- AST (original):
+    #{original_ast_pretty}
+
+    -- AST (expandida):
+    #{expanded_ast_pretty}
+
+    -- Código gerado (após expansão):
+    #{generated_code}
+    === /MacroDbg ===
+
+    """)
+
+    IO.puts("Codigo normal:
+      #{generated_code_normal}
+      ")
+
+    block
+  end
+end
