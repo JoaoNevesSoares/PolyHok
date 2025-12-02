4b592311f6a6d63fe4d1f2299c73fc141aeff7a9
jans@ubuntu <jansoares@inf.ufpel.edu.br>
2025-12-02 10:38:26 -0300

    Feat: Implemented fused with device functions

--- lib/fusion.ex
+++ lib/fusion.ex
@@ -80,51 +80,98 @@ defmodule Fusion do
     end
   end
 
-defp build_fused_quote(data_ast, phok_f_wrapped, phok_g_wrapped, _caller_env) do
-  with {:ok, f_ast} <- extract_phok(phok_f_wrapped),
-       {:ok, g_ast} <- extract_phok(phok_g_wrapped) do
-    {f_param, f_body} = peel_fn(f_ast)
-    {g_param, g_body} = peel_fn(g_ast)
+  defp extract_device_name({:/, _, [inner, 1]}) do
+    case inner do
+      {{:., _, [_mod_ast, fun_name]}, _, _} when is_atom(fun_name) ->
+        fun_name
 
-    f_name =
-      case f_param do
-        {name, _m, _ctx} when is_atom(name) -> name
-        _ -> raise ArgumentError, "Parametro de f não reconhecido"
-      end
+      {fun_name, _, _} when is_atom(fun_name) ->
+        fun_name
+    end
+  end
 
-    g_name =
-      case g_param do
-        {name, _m, _ctx} when is_atom(name) -> name
-        _ -> raise ArgumentError, "Parametro de g não reconhecido"
-      end
+  defp build_fused_device_quote(data_ast, f_device_ast, [g_device_ast]) do
+    f_name = extract_device_name(f_device_ast)
+    g_name = extract_device_name(g_device_ast)
 
-    # f_body continua em termos de f_name (ex: x + 1.0)
-    # g_body(y) vira g_body(f_body(x)) substituindo y por f_body
-    composed_body =
-      g_body
-      |> replace_var(g_name, f_body)
-      |> normalize_kernel_ops()   # se você ainda estiver usando
-      # |> demote_int_floats()      # opcional, se precisar
+    # variáveis da DSL
+    x_var = {:x, [], nil}
+    y_var = {:y, [], nil}
+
+    # type y int
+    y_type_ast = {:y, [], [{:int, [], []}]}
+    type_y = {:type, [], [y_type_ast]}
+
+    # y = f(x)
+    f_call = {f_name, [], [x_var]}
+    assign_y = {:=, [], [y_var, f_call]}
+
+    # return g(y)
+    g_call = {g_name, [], [y_var]}
+    return_stmt = {:return, [], [g_call]}
+
+    body_block = {:__block__, [], [type_y, assign_y, return_stmt]}
+
+    fused_fn_ast = {:fn, [], [{:->, [], [[x_var], body_block]}]}
 
     quote do
       Ske.map(
         unquote(data_ast),
-        PolyHok.phok(fn unquote(f_param) ->
-          unquote(composed_body)
-        end)
+        PolyHok.phok(unquote(fused_fn_ast))
       )
     end
-  else
-    _ ->
-      raise ArgumentError, "with_fusion/1 requer PolyHok.phok(fn ... end)"
   end
-end
+
+  defp build_fused_quote(data_ast, phok_f_wrapped, phok_g_wrapped, _caller_env) do
+    with {:ok, f_ast} <- extract_phok(phok_f_wrapped),
+         {:ok, g_ast} <- extract_phok(phok_g_wrapped) do
+      {f_param, f_body} = peel_fn(f_ast)
+      {g_param, g_body} = peel_fn(g_ast)
+
+      f_name =
+        case f_param do
+          {name, _m, _ctx} when is_atom(name) -> name
+          _ -> raise ArgumentError, "Parametro de f não reconhecido"
+        end
+
+      g_name =
+        case g_param do
+          {name, _m, _ctx} when is_atom(name) -> name
+          _ -> raise ArgumentError, "Parametro de g não reconhecido"
+        end
+
+      # f_body continua em termos de f_name (ex: x + 1.0)
+      # g_body(y) vira g_body(f_body(x)) substituindo y por f_body
+      composed_body =
+        g_body
+        |> replace_var(g_name, f_body)
+        # se você ainda estiver usando
+        |> normalize_kernel_ops()
+
+      # |> demote_int_floats()      # opcional, se precisar
+
+      quote do
+        Ske.map(
+          unquote(data_ast),
+          PolyHok.phok(fn unquote(f_param) ->
+            unquote(composed_body)
+          end)
+        )
+      end
+    else
+      _ ->
+        raise ArgumentError, "with_fusion/1 requer PolyHok.phok(fn ... end)"
+    end
+  end
 
   defmacro with_fusion({:|>, _m, [lhs, rhs]}) do
     lhs_match = extract_ske_map(lhs)
     rhs_match = extract_ske_map(rhs)
 
     case {lhs_match, rhs_match} do
+      {{:ok, [data_ast, {:&, _meta, [f_device]}]}, {:ok, [{:&, _, g_device}]}} ->
+        build_fused_device_quote(data_ast, f_device, g_device)
+
       {{:ok, [data_ast, phok_f_ast]}, {:ok, [phok_g_ast]}} ->
         build_fused_quote(data_ast, phok_f_ast, phok_g_ast, __CALLER__)
 
