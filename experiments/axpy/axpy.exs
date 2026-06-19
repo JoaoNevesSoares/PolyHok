a72fb5e99ac9f7436b15bb3220de6c1435af99c9
JoaoNevesSoares <jans@inf.ufpel.edu.br>
2026-06-19 05:52:47 -0300

    refactoring axpy code

--- experiments/axpy/axpy.exs
+++ experiments/axpy/axpy.exs
@@ -1,17 +1,22 @@
 require PolyHok
-# require Fusion
-
-n = 268_435_456
-
-dev_a = PolyHok.random_gnx(0, 10, n)
-dev_b = PolyHok.random_gnx(0, 10, n)
-
+require PolyHokInspect
+require Fusion
 use Ske
-a = 1.1
-start_time = System.monotonic_time()
-res = Ske.map2(dev_a, dev_b, PolyHok.clo(fn x, y -> a * x + y end))
-end_time = System.monotonic_time()
-res_cpu = PolyHok.get_gnx(res)
-elapsed_ms = System.convert_time_unit(end_time - start_time, :native, :millisecond)
-IO.inspect(res_cpu)
-IO.puts("time #{elapsed_ms}")
+
+PolyHok.defmodule Axpy do
+  defd axpy(x, y) do
+    3.1415 * x + y
+  end
+
+  def unfused(a, b, c) do
+    Ske.map2(a, b, &Axpy.axpy/2)
+    |> Ske.map2(c, &Axpy.axpy/2)
+  end
+
+  def fused(a, b, c) do
+      Fusion.with_fusion(
+        Ske.map2(a, b, &Axpy.axpy/2)
+        |> Ske.map2(c, &Axpy.axpy/2)
+      )
+  end
+end
